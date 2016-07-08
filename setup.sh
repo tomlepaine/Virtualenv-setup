d8767965f4d727c0baeae8b95e0bab61fec1877d
Tom Le Paine <tom.le.paine@gmail.com>
2016-07-08 16:17:50 -0500

    Add jupyter profile seteup.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+# run from env/bin
 
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-
-wget https://gist.githubusercontent.com/miki725/9783474/raw/70a2da69bdfe4ceadac4fb6c153ae6e4f94eac27/.bash_prompt.sh -O bash-prompt.sh
+wget https://gist.githubusercontent.com/miki725/9783474/raw/70a2da69bdfe4ceadac4fb6c153ae6e4f94eac27/.bash_prompt.sh \
+    -O bash-prompt.sh
+mkdir ../jupyter_profile
+wget  \
+    -O ./jupyter_profile/jupyter_notebook_config.py
 
 echo "source $VIRTUAL_ENV/bin/git-completion.bash" >> activate
 echo "source $VIRTUAL_ENV/bin/bash-prompt.sh" >> activate
+echo "export JUPYTER_CONFIG_DIR=$VIRTUAL_ENV/jupyter_profile" >> activate
+
